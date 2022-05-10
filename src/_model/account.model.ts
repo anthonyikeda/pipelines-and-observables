ac53846f1f48428a25ebf01dc93fcf7cdfdc357b
Anthony Ikeda <anthony.ikeda@gmail.com>
2022-05-10 09:07:42 -0700

    Trying to modularize the files Moving the standalone statements to classes

---
+++ src/_model/account.model.ts
@@ -0,0 +1,11 @@
+export interface LinkedAccount {
+  id: number;
+  email: string;
+  firstname: string;
+  lastname: string;
+}
+
+export interface User {
+  id: number;
+  email: string;
+}
--- src/auth-pipeline.ts
+++ src/auth-pipeline.ts
@@ -1,53 +1,48 @@
 import { switchMap, map, of, Observable } from "rxjs";
+import { LinkedAccount, User } from "./_model";
 
-interface LinkedAccount {
-  id: number;
-  email: string;
-  firstname: string;
-  lastname: string;
+export class AuthValidationService {
+  // constructor() {}
+
+  getLinkedAccounts(email: string): Observable<LinkedAccount> {
+    const acct: LinkedAccount = {
+      id: 4092,
+      email: email,
+      firstname: "Joseph",
+      lastname: "Matthews"
+    };
+
+    return of(acct);
+  }
+
+  validateLogin(): void {
+    let currUser: User = {
+      id: 4000,
+      email: "user@home.com"
+    };
+
+    let isAuthenticated$: Observable<boolean> = of(true);
+    let user$ = of(currUser);
+
+    let process$ = isAuthenticated$.pipe(
+      switchMap((isLoggedIn: boolean) => {
+        if (isLoggedIn) {
+          console.log("User is logged in");
+          return user$.pipe(map((item: User) => item?.email));
+        } else {
+          console.log("User is not logged in");
+          return of("");
+        }
+      }),
+      map((email: string) => {
+        console.log(`Email is ${email}`);
+        return email;
+      }),
+      switchMap((email: string) => this.getLinkedAccounts(email))
+    );
+
+    process$.subscribe((data: LinkedAccount[]) => {
+      console.log(data);
+    });
+  }
 }
-
-interface User {
-  id: number;
-  email: string;
-}
-
-let getLinkedAccounts = (email: string): Observable<LinkedAccount> => {
-  const acct: LinkedAccount = {
-    id: 4092,
-    email: email,
-    firstname: "Joseph",
-    lastname: "Matthews"
-  };
-
-  return of(acct);
-};
-
-let currUser: User = {
-  id: 4000,
-  email: "user@home.com"
-};
-
-let isAuthenticated$ = of(true);
-let user$ = of(currUser);
-
-let process$ = isAuthenticated$.pipe(
-  switchMap((isLoggedIn) => {
-    if (isLoggedIn) {
-      console.log("User is logged in");
-      return user$.pipe(map((item) => item?.email));
-    } else {
-      console.log("User is not logged in");
-      return of("");
-    }
-  }),
-  map((email) => {
-    console.log(`Email is ${email}`);
-    return email;
-  }),
-  switchMap((email) => getLinkedAccounts(email))
-);
-
-process$.subscribe((data) => {
-  console.log(data);
-});
